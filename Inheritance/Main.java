45c899d0ce75b34cb848e8061dab4f6f9fcf787e
hndyn12 <145266406+hndyn12@users.noreply.github.com>
2023-10-30 13:47:21 +0700

    Main.java

--- Inheritance/Main.java
+++ Inheritance/Main.java
@@ -13,14 +13,26 @@ public class Main {
     Enemy monster = new Enemy();
     
     Zombie zumbi = new Zombie();
+    zumbi.name ="Zuombie";
+    zumbi.hp= 80;
+    zumbi.attackpoin=15;
+    System.out.println(zumbi.name +" Healt Point = "+zumbi.hp + " Attack poin = "+zumbi.attackpoin);
     zumbi.attack();
     zumbi.walk();
     
     Pocong hantupocong = new Pocong();
+    hantupocong.name ="Puocong";
+    hantupocong.hp= 85;
+    hantupocong.attackpoin=10;
+    System.out.println(hantupocong.name +" Healt Point = "+hantupocong.hp + " Attack poin = "+hantupocong.attackpoin);
     hantupocong.attack();
     hantupocong.jump();
     
     Burung garuda = new Burung();
+    garuda.name ="Guaruda";
+    garuda.hp= 100;
+    garuda.attackpoin=5;  
+    System.out.println(garuda.name +" Healt Point = "+garuda.hp + " Attack poin = "+garuda.attackpoin);
     garuda.attack();
     garuda.walk();
     garuda.jump();
