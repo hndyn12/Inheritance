8ae6eed9579af795d0e29662bd23b95ad38dc2e6
hndyn12 <145266406+hndyn12@users.noreply.github.com>
2023-10-30 13:30:07 +0700

    Persegipanjang.java

--- InheritanceBangunDatar/Persegipanjang.java
+++ InheritanceBangunDatar/Persegipanjang.java
@@ -11,4 +11,17 @@ package InheritanceBangunDatar;
 public class Persegipanjang extends BangunDatar{
     float panjang;
     float lebar;
+    @Override
+    float luas(){
+        float luas = panjang * lebar;
+        System.out.println("Luas Persegi Panjang:" + luas);
+        return luas;
+    }
+    
+    @Override
+    float keliling(){
+        float keliling = 2*panjang + 2*lebar;
+        System.out.println("Keliling Persegi Panjang: " + keliling);
+        return keliling;
+    }
 }
