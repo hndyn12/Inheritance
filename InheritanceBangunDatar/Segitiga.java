06a12fc4fc87d8df4aa7b68f02c3c659a531f132
hndyn12 <145266406+hndyn12@users.noreply.github.com>
2023-10-30 13:30:40 +0700

    Segitiga.java

--- InheritanceBangunDatar/Segitiga.java
+++ InheritanceBangunDatar/Segitiga.java
@@ -11,4 +11,18 @@ package InheritanceBangunDatar;
 public class Segitiga extends BangunDatar {
     float tinggi;
     float alas;
+    float miring;
+    @Override
+    float luas() {
+        float luas;
+        luas =(alas * tinggi)/2;
+        System.out.println("Luas Segitiga: " + luas);
+        return luas;
+    }   
+    @Override
+    float keliling() {
+        float keliling = tinggi + alas + miring;
+        System.out.println("Keliling Segitiga: " + keliling);
+        return keliling;
+    }   
 }
