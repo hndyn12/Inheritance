77b10473ae3720e0d24e12e10c0c49d9b86fb886
hndyn12 <145266406+hndyn12@users.noreply.github.com>
2023-10-30 13:27:58 +0700

    Lingkaran.java

--- InheritanceBangunDatar/Lingkaran.java
+++ InheritanceBangunDatar/Lingkaran.java
@@ -10,4 +10,17 @@ package InheritanceBangunDatar;
  */
 public class Lingkaran extends BangunDatar{
     float r;
+    @Override
+    float luas(){
+        float luas = (float) (Math.PI * r * r);
+        System.out.println("Luas lingkaran: " + luas);
+        return luas;
+    }
+    
+    @Override
+    float keliling(){
+        float keliling = (float) (2 * Math.PI * r);
+        System.out.println("Keliling Lingkaran: " + keliling);
+        return keliling;
+    }
 }
