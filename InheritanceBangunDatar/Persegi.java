2624d3f952d1b097e917da7dcbe09c64f92673fc
hndyn12 <145266406+hndyn12@users.noreply.github.com>
2023-10-30 13:28:53 +0700

    Persegi.java

--- InheritanceBangunDatar/Persegi.java
+++ InheritanceBangunDatar/Persegi.java
@@ -10,4 +10,16 @@ package InheritanceBangunDatar;
  */
 public class Persegi extends BangunDatar {
     float sisi;
+    @Override
+    float luas(){
+        float luas = sisi * sisi;
+        System.out.println("Luas Persegi: " + luas);
+        return luas;
+    }
+    @Override
+    float keliling(){
+        float keliling = 4 * sisi;
+        System.out.println("Keliling Persegi: " + keliling);
+        return keliling;
+    }
 }
